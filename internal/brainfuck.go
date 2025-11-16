53a8a4b857366a8225a254cb41017476be85edbf
adityakiran1423 <adi.kiran@gmail.com>
2025-11-16 18:39:28 +0530

    (internal): added implementation for all commands except loops

--- internal/brainfuck.go
+++ internal/brainfuck.go
@@ -1,14 +1,14 @@
 package internal
 
 type Program struct {
-	ProgramArray [30000]uint32
-	ProgramFilePath string
-	ProgramCounter int
+	ProgramArray       [30000]uint32
+	ProgramFilePath    string
+	InstructionPointer int
 }
 
-func New(brainfuckFilePath string) (*Program) {
-	return &Program {
-		ProgramFilePath: brainfuckFilePath,
-		ProgramCounter: 0,
+func New(brainfuckFilePath string) *Program {
+	return &Program{
+		ProgramFilePath:    brainfuckFilePath,
+		InstructionPointer: 0,
 	}
 }
--- internal/interpreter.go
+++ internal/interpreter.go
@@ -1,35 +1,100 @@
 package internal
 
-import(
-	"os"
+import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 
 	u "example.com/gofck/utils"
 )
 
-func Run(program *Program) {
-	valid := validate(program)
+func Run(p *Program) {
+	valid := validate(p)
 	if !valid {
-		fmt.Printf("Error while loading file : %s\n", program.ProgramFilePath)
+		fmt.Printf("Error while loading file : %s\n", p.ProgramFilePath)
 		os.Exit(0)
 	}
 	fmt.Println("Program is valid")
+
+	// actual running
+	code, _ := os.ReadFile(p.ProgramFilePath)
+	i := 0
+	for i < len(code) {
+		switch string(code[i]) {
+		case "+":
+			if p.ProgramArray[p.InstructionPointer] == 255 {
+				p.ProgramArray[p.InstructionPointer] = 0
+			} else {
+				p.ProgramArray[p.InstructionPointer]++
+			}
+
+		case "-":
+			if p.ProgramArray[p.InstructionPointer] == 0 {
+				p.ProgramArray[p.InstructionPointer] = 255
+			} else {
+				p.ProgramArray[p.InstructionPointer]--
+			}
+
+		case ">":
+			if p.InstructionPointer == 30000 {
+				fmt.Println("Error : Memory Overflow")
+				os.Exit(0)
+			}
+			p.InstructionPointer++
+
+		case "<":
+			if p.InstructionPointer == 0 {
+				fmt.Println("Error : Memory Overflow")
+				os.Exit(0)
+			}
+			p.InstructionPointer--
+
+		case ".":
+			fmt.Println("printing using .")
+			fmt.Print(p.ProgramArray[p.InstructionPointer])
+
+		case ",":
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("Enter text :")
+			text, _ := reader.ReadString('\n')
+
+			if inputInt, err := strconv.Atoi(text); err == nil {
+				int32Input := uint32(inputInt)
+				p.ProgramArray[p.InstructionPointer] = int32Input
+			} else {
+				fmt.Println("Error during input")
+				os.Exit(0)
+			}
+
+		case "[":
+
+
+		case "]":
+
+		default:
+			fmt.Println("invalid operation")
+		}
+		i++
+	}
+	fmt.Println()
 }
 
-func validate(program *Program) (bool) {
-	filePathValidateResult := validateFilePath(program.ProgramFilePath)
+func validate(p *Program) bool {
+	filePathValidateResult := validateFilePath(p.ProgramFilePath)
 
 	if filePathValidateResult {
-		bracketValidateResult := validateBrackets(program.ProgramFilePath)
-		if bracketValidateResult { return true }
+		bracketValidateResult := validateBrackets(p.ProgramFilePath)
+		if bracketValidateResult {
+			return true
+		}
 	}
 
 	return false
 }
 
-func validateFilePath(brainfuckFilePath string) (bool) {
+func validateFilePath(brainfuckFilePath string) bool {
 	_, err := os.ReadFile(brainfuckFilePath)
-	// os.Stdout.Write(data)
 
 	if err != nil {
 		fmt.Println("Error, file not found")
@@ -38,12 +103,14 @@ func validateFilePath(brainfuckFilePath string) (bool) {
 	return true
 }
 
-func validateBrackets(brainfuckFilePath string) (bool) {
+func validateBrackets(brainfuckFilePath string) bool {
 	codeBytes, _ := os.ReadFile(brainfuckFilePath)
 	code := string(codeBytes)
-	fmt.Println(code)
+	// fmt.Println(code)
+
 	i := 0
 	var stack u.Stack
+
 	for i < len(code) {
 		if string(code[i]) == "[" {
 			stack.Push(string(code[i]))
@@ -53,5 +120,11 @@ func validateBrackets(brainfuckFilePath string) (bool) {
 		}
 		i++
 	}
-	return stack.IsEmpty()
+
+	if stack.IsEmpty() {
+		return true
+	}
+
+	fmt.Println("Error, brackets are not balanced")
+	return false
 }
